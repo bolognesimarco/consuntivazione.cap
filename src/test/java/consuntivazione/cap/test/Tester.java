6ce6a8785401ba2672c396df8412017a9883b8a2
bolognesimarco <bolognesi.marco@gmail.com>
2014-10-03 12:10:07 +0200

    Prova

--- src/test/java/consuntivazione/cap/test/Tester.java
+++ src/test/java/consuntivazione/cap/test/Tester.java
@@ -3,13 +3,18 @@ package consuntivazione.cap.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import consuntivazione.cap.service.TimeSheetServiceImpl;
+import consuntivazione.cap.service.TimeSheetService;
+import consuntivazione.cap.vo.WorkerVO;
 
 public class Tester {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		ctx.getBean(TimeSheetServiceImpl.class);
+		TimeSheetService tss = ctx.getBean(TimeSheetService.class);
+		WorkerVO vo = new WorkerVO();
+		vo.setName("Bolognesi Marco");
+		tss.newWorker(vo);
+		System.exit(0);
 	}
 
 }
